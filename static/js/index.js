0b573aa3bdd590da2be0736a2ce8c28d37d47a7d
Finlay <rossf.seg@gmail.com>
2020-05-05 17:23:43 -0500

    display_name in localstorage

---
+++ static/js/index.js
@@ -0,0 +1,16 @@
+// Set starting value of display_name to empty string
+if (!localStorage.getItem('display_name'))
+	localStorage.setItem('display_name', "No Name Set");
+
+// Load current value of  counter
+document.addEventListener('DOMContentLoaded', () => {
+	document.querySelector('#display_name').innerHTML = localStorage.getItem('display_name');
+	
+	//update display_name with text input
+	document.querySelector('#form').onsubmit = () => {
+		const name = document.querySelector('#name').value;
+		
+		document.querySelector('#display_name').innerHTML = name;
+		localStorage.setItem('display_name', name);
+	};
+});
