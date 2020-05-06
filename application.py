55c692c282d5496ffcc722e5bf478b1609020c85
Finlay <rossf.seg@gmail.com>
2020-05-06 12:39:52 -0500

    initial attempt

--- application.py
+++ application.py
@@ -7,7 +7,12 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+display_name = []
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return render_template("index.html", display_name=display_name)
+
+@socketio.on("register display_name")
+def name(data):
+    display_name = data["display_name"]
--- static/js/index.js
+++ static/js/index.js
@@ -6,13 +6,20 @@ if (!localStorage.getItem('display_name'))
 document.addEventListener('DOMContentLoaded', () => {
 	var display_name = localStorage.getItem('display_name');
 	
+	// Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 	
-	//update display_name with text input
-	document.querySelector('#form').onsubmit = () => {
-		const name = document.querySelector('#name').value;
-		display_name = name;
-		localStorage.setItem('display_name', name);
-	};
+	// When connected
+	socket.on('connect', () => {
+		
+		//update display_name with text input
+		document.querySelector('#form').onsubmit = () => {
+			const name = document.querySelector('#name').value;
+			display_name = name;
+			localStorage.setItem('display_name', name);
+			socket.emit('register display_name', {'display_name'}: display_name);
+		};
+	});
 	
 	document.querySelector('#display_name').innerHTML = display_name;
 });
