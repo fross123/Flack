2ce6a88760ada16ddcd0576ce8746f02ea54e717
Finlay <rossf.seg@gmail.com>
2020-05-14 11:09:35 -0500

    no server for current user b/c shared. List of current users instead

--- application.py
+++ application.py
@@ -8,21 +8,22 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 display_names = []
-current_user = []
+current_users = []
 
 @app.route("/")
 def index():
-    return render_template('index.html', display_names=display_names, current_user=current_user)
+    return render_template('index.html', display_names=display_names)
 
 @socketio.on("new_user")
 def new_user(data):
     display_name = data["display_name"]
     display_names.append(display_name)
-    current_user.clear()
-    current_user.append(display_name)
+
+    emit("user_signed_in", current_users, broadcast=True)
 
 @socketio.on("returning_user")
 def returning_user(data):
     display_name = data["display_name"]
-    current_user.clear()
-    current_user.append(display_name)
+    current_users.append(display_name)
+
+    emit("user_signed_in", current_users, broadcast=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,29 +1,29 @@
 if (!localStorage.getItem('display_name')) {
     localStorage.setItem('display_name', "");
-    localStorage.setItem('display_name_created', false);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 
     var display_name = localStorage.getItem('display_name');
-    var display_name_created = localStorage.getItem('display_name_created');
 
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
     socket.on('connect', () => {
-        if (display_name_created != false) {
-            socket.emit('returning_user', {display_name: display_name});
+        if (display_name != "") {
+            socket.emit('returning_user', {display_name: localStorage.getItem('display_name')});
         }
 
         document.querySelector('#form').onsubmit = () => {
             const name = document.querySelector('#name').value;
             display_name = name;
-            display_name_created = true;
 
             localStorage.setItem('display_name', display_name);
-            localStorage.setItem('display_name_created', display_name_created);
             socket.emit('new_user', {display_name: display_name});
         };
+    });
+
+    socket.on('user_signed_in', data => {
+        document.querySelector('#current_user').innerHTML = data;
     });
 });
