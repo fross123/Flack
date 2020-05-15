9b6df5aeefe935ecca1c149c7421f55bf81dd274
Finlay <rossf.seg@gmail.com>
2020-05-15 09:34:52 -0500

    channel adding v.1 - no work

--- application.py
+++ application.py
@@ -9,10 +9,11 @@ socketio = SocketIO(app)
 
 display_names = []
 current_users = []
+channels = []
 
 @app.route("/")
 def index():
-    return render_template('index.html', display_names=display_names)
+    return render_template('index.html', display_names=display_names, current_users=current_users, channels=channels)
 
 @socketio.on("new_user")
 def new_user(data):
@@ -24,6 +25,15 @@ def new_user(data):
 @socketio.on("returning_user")
 def returning_user(data):
     display_name = data["display_name"]
-    current_users.append(display_name)
+
+    if display_name not in current_users:
+        current_users.append(display_name)
 
     emit("user_signed_in", current_users, broadcast=True)
+
+@socketio.on("new_channel")
+def new_channel(data):
+    channel_name = data["channel_name"]
+    channels.append(channel_name)
+
+    emit("channel_created", channels, broadcast=True)
--- static/js/index.js
+++ static/js/index.js
@@ -5,6 +5,7 @@ if (!localStorage.getItem('display_name')) {
 document.addEventListener('DOMContentLoaded', () => {
 
     var display_name = localStorage.getItem('display_name');
+    var channel_name = "";
 
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
@@ -13,29 +14,62 @@ document.addEventListener('DOMContentLoaded', () => {
         if (display_name != "") {
             socket.emit('returning_user', {display_name: localStorage.getItem('display_name')});
         }
-
-        document.querySelector('#form').onsubmit = () => {
-            const name = document.querySelector('#name').value;
-            display_name = name;
-
-            localStorage.setItem('display_name', display_name);
-            socket.emit('new_user', {display_name: display_name});
-        };
+        else if (display_name == "") {
+            const add_channel_button = document.querySelector("#add_channel_button");
+            add_channel_button.disabled = true;
+        }
     });
 
     socket.on('user_signed_in', data => {
+        // Add user to dom
         data.forEach(add_user);
+
+        // disable submit new user button
         const add_user_button = document.querySelector('#add_user_button');
         add_user_button.disabled = true;
+
+        // enable create channel button
+        const add_channel_button = document.querySelector("#add_channel_button");
+        add_channel_button.disabled = false;
     });
+
+    socket.on('channel_created', data => {
+        // add channel to channel List
+        data.forEach(add_channel);
+    });
+
+    document.querySelector('#form').onsubmit = () => {
+        const name = document.querySelector('#name').value;
+        display_name = name;
+
+        localStorage.setItem('display_name', display_name);
+        socket.emit('new_user', {display_name: display_name});
+    };
+
+    document.querySelector('#new_channel_form').onsubmit = () => {
+        const channel = document.querySelector('#channel_name').value;
+        channel_name = channel;
+
+        socket.emit('new_channel', {channel_name: channel_name});
+    };
 });
 
-// Add a new post with given contents to DOM.
+// Add a new channel to DOM.
+const channel_template = Handlebars.compile(document.querySelector('#add_channel').innerHTML);
+function add_channel(contents) {
+    // Create new channel.
+    const channel = channel_template({'contents': contents});
+
+    // Add channel to DOM.
+    document.querySelector('#channel_list').innerHTML += channel;
+};
+
+// Add a new user to DOM.
 const user_template = Handlebars.compile(document.querySelector('#user').innerHTML);
 function add_user(contents) {
-    // Create new post.
+    // Create new user.
     const user = user_template({'contents': contents});
 
-    // Add post to DOM.
+    // Add user to DOM.
     document.querySelector('#current_users').innerHTML += user;
-}
+};
