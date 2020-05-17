0892d93d83dfd50b421812a5064209a461bb891f
Finlay <rossf.seg@gmail.com>
2020-05-16 22:14:13 -0500

    added secret key

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = '247e489ce72804f050ad8ac1b25cebfa'
 socketio = SocketIO(app)
 
 display_names = []
